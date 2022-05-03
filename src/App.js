8546ec573a9e8582c0d8921ab85de3d167020b5c
zeed3v <felipebrionese@gmail.com>
2022-05-02 20:56:10 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import {
+  Home,
+  About, 
+  Events, 
+  Contact, 
+  Whoops404,
+  Services,
+  CompanyHistory,
+  Location
+} from "./pages";
 
-function App({ }) {
-
+function App() {
   return (
     <div>
-      <h1>Hello React Testing Library</h1>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<About />}>
+            <Route path='services' element={<Services />} />
+            <Route path='history' element={<CompanyHistory />} />
+            <Route path='location' element={<Location />} />
+          </Route>
+          <Route path='/events' element={<Events />}/>
+          <Route path='/contact' element={<Contact />}/>
+          <Route path='*' element={<Whoops404 />}/>
+        </Routes>
     </div>
   );
 }
--- src/pages.js
+++ src/pages.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link, useLocation, Outlet } from "react-router-dom";
 
 export function Home() {
     return (
         <div>
             <h1>[Company Website]</h1>
+            <nav>
+                <Link to="about">About</Link>
+                <Link to="events">Events</Link>
+                <Link to="contact">Contact</Link>
+            </nav>
         </div>
     );
 }
@@ -12,6 +18,31 @@ export function About() {
     return (
         <div>
             <h1>[About]</h1>
+            <Outlet />
+        </div>
+    );
+}
+
+export function Services() {
+    return (
+        <div>
+            <h2>Our Services</h2>
+        </div>
+    );
+}
+
+export function CompanyHistory() {
+    return (
+        <div>
+            <h2>Our Company History</h2>
+        </div>
+    );
+}
+
+export function Location() {
+    return (
+        <div>
+            <h2>Our Location</h2>
         </div>
     );
 }
@@ -30,4 +61,14 @@ export function Contact() {
             <h1>[Contact]</h1>
         </div>
     );
+}
+
+export function Whoops404() {
+    let location = useLocation();
+    console.log(location);
+    return (
+        <div>
+            <h1>Resource not found at {location.pathname}</h1>
+        </div>
+    )
 }
